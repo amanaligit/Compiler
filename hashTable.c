bdbe991b677f89f13a45063f052d2ce125f2ec39
Sabhya Kapur <sabhya.1802@gmail.com>
2022-03-02 02:12:22 +0530

    lot of work pending for hashTable

--- hashTable.c
+++ hashTable.c
@@ -1,5 +1,34 @@
 #include<hashTable.h>
 
 int hashCode(char* str){
-    
+    int p = 31;
+    int m = 1e9+9;
+    long long ans = 0;
+    int len = strlen(str);
+    int currMult = 1;
+    for(int i = 0; i<len; ++i){
+        ans = (ans+ str[i]*currMult)%m;
+        currMult = (currMult*p)%m;
+    }
+    return ans%TABLE_SIZE;
+}
+
+void constructHashTable(hashEntry hashTable[]){
+    for(int i = 0; i<TABLE_SIZE; ++i){
+        hashTable[i].str = NULL;
+        hashTable[i].token = -1;
+        hashTable[i].next = NULL;
+    }
+}
+
+void pushHashTable(char* str, Tk tokenType, hashEntry hashTable[]){
+    int hash = hashCode(str);
+    if(!hashTable[hash].str){ //the cell at this hash is empty
+        hashTable[hash].str = (char*)malloc(sizeof(char)*strlen(str)+1);
+        strcpy(hashTable[hash].str, str);
+        hashTable[hash].token = tokenType;
+    }
+    else{
+        
+    }
 }
--- hashTable.h
+++ hashTable.h
@@ -6,30 +6,29 @@
 #define BUFF_SIZE 4096
 #define TABLE_SIZE 1000
 #define MAX_ID_SIZE 20
-#define PRE_COMPUTING_MULTIPLE_COUNT 12
 
-typedef struct entry{
-    char* str; //the string corresponding to the lexeme
-    int val; //the value for the lexeme
-    struct entry* next; //if separate chaining has to be employed for collision management
-
-} hashEntry;
-
-hashEntry hashMap[TABLE_SIZE];
 char buf1[BUFF_SIZE+1];
 char buf2[BUFF_SIZE+1];
-long long hashMultiples[PRE_COMPUTING_MULTIPLE_COUNT];
 
-typedef enum tk{
+typedef enum {
     TK_ASSIGNOP, TK_COMMENT, TK_FIELDID, TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_RUID, TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_UNION, TK_ENDUNION, TK_DEFINETYPE, TK_AS, TK_TYPE, 
     TK_MAIN, TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_SQL, TK_SQR, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_COMMA, TK_SEM, TK_COLON, TK_DOT, TK_ENDWHILE, TK_OP, TK_CL, TK_IF, TK_THEN,
     TK_ENDIF, TK_READ, TK_WRITE, TK_RETURN, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV, TK_CALL, TK_RECORD, TK_ENDRECORD, TK_ELSE, TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ, TK_GT, TK_LE,
     TK_NE
 } Tk;
 
+typedef struct entry{
+    char* str; //the string corresponding to the lexeme
+    Tk token; //the value for the lexeme
+    struct entry* next; //if separate chaining has to be employed for collision management
+
+} hashEntry;
+
+hashEntry hashMap[TABLE_SIZE];
+
 int hashCode(char* str);
 void constructHashTable(hashEntry hashTable[]);
-int pushHashTable(char* str, Tk tokenType, hashEntry hashTable[]);
+void pushHashTable(char* str, Tk tokenType, hashEntry hashTable[]);
 void pushKeyword(hashEntry hashTable[]);
 
 #endif
