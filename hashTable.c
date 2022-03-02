e1902086dad43ff26243e6f68900714cb0f50fc3
Sabhya Kapur <sabhya.1802@gmail.com>
2022-03-02 13:57:27 +0530

    moving on to lexer

--- hashTable.c
+++ hashTable.c
@@ -1,34 +1,132 @@
-#include<hashTable.h>
+#include "hashTable.h"
 
-int hashCode(char* str){
-    int p = 31;
-    int m = 1e9+9;
-    long long ans = 0;
-    int len = strlen(str);
-    int currMult = 1;
-    for(int i = 0; i<len; ++i){
-        ans = (ans+ str[i]*currMult)%m;
-        currMult = (currMult*p)%m;
+int hashCode(char *str)
+{
+    int length = strlen(str);
+    ll mod = 1e9 + 9;
+    ll p = 1e6 + 7;
+    ll curMult = 0;
+    int iterator = 0;
+    for (iterator = 0; iterator < length; iterator++)
+    {
+        curMult = ((curMult * p) + str[iterator]) % mod;
     }
-    return ans%TABLE_SIZE;
+    curMult %= TABLE_SIZE;
+    return curMult;
 }
 
-void constructHashTable(hashEntry hashTable[]){
-    for(int i = 0; i<TABLE_SIZE; ++i){
+void constructHashTable(hashEntry hashTable[])
+{
+    for (int i = 0; i < TABLE_SIZE; ++i)
+    {
         hashTable[i].str = NULL;
-        hashTable[i].token = -1;
         hashTable[i].next = NULL;
     }
 }
 
-void pushHashTable(char* str, Tk tokenType, hashEntry hashTable[]){
+void pushHashTable(char *str, Tk tokenType, hashEntry hashTable[])
+{
     int hash = hashCode(str);
-    if(!hashTable[hash].str){ //the cell at this hash is empty
-        hashTable[hash].str = (char*)malloc(sizeof(char)*strlen(str)+1);
+    // printf("%d\n", hash);
+    if (!hashTable[hash].str)
+    { // the cell at this hash is empty
+        hashTable[hash].str = (char *)malloc(sizeof(char) * strlen(str) + 1);
         strcpy(hashTable[hash].str, str);
+        // hashTable[hash].str = str;
         hashTable[hash].token = tokenType;
     }
-    else{
-        
+    else
+    {
+        hashEntry *trav = (hashEntry *)malloc(sizeof(hashEntry));
+        while (trav->next)
+        {
+            trav = trav->next;
+        }
+        trav->next = (hashEntry *)malloc(sizeof(hashEntry));
+        trav = trav->next;
+        trav->str = (char *)malloc(sizeof(char) * strlen(str) + 1);
+        strcpy(trav->str, str);
+        // hashTable[hash].str = str;
+        trav->token = tokenType;
+        trav->next = NULL;
     }
+}
+
+hashRet* initHashRet(){
+    hashRet* obj = (hashRet*)malloc(sizeof(hashRet));
+    obj->found = FALSE;
+    obj->ent = NULL;
+    return obj;
+}
+
+hashRet* search(char* str, hashEntry hashMap[]){
+    int hash = hashCode(str);
+    hashRet* obj = initHashRet();
+    if(!hashMap[hash].str){
+        hashEntry* trav = &hashMap[hash];
+        while(trav){
+            if(!strcmp(str, trav->str)){
+                obj->found = TRUE;
+                obj->ent = trav;
+            }
+            trav = trav->next;
+        }
+    }
+    return obj;
+}
+
+void pushKeyword(hashEntry hashTable[])
+{
+    constructHashTable(hashTable);
+    pushHashTable("with", TK_WITH, hashTable);
+    pushHashTable("parameters", TK_PARAMETERS, hashTable);
+    pushHashTable("end", TK_END, hashTable);
+    pushHashTable("While", TK_WHILE, hashTable);
+    pushHashTable("Union", TK_UNION, hashTable);
+    pushHashTable("endunion", TK_ENDUNION, hashTable);
+    pushHashTable("definetype", TK_DEFINETYPE, hashTable);
+    pushHashTable("as", TK_AS, hashTable);
+    pushHashTable("Type", TK_TYPE, hashTable);
+    pushHashTable("_main", TK_MAIN, hashTable);
+    pushHashTable("Global", TK_GLOBAL, hashTable);
+    pushHashTable("parameter", TK_PARAMETER, hashTable);
+    pushHashTable("List", TK_LIST, hashTable);
+    pushHashTable("Input", TK_INPUT, hashTable);
+    pushHashTable("Output", TK_OUTPUT, hashTable);
+    pushHashTable("Int", TK_INT, hashTable);
+    pushHashTable("Real", TK_REAL, hashTable);
+    pushHashTable("endwhile", TK_ENDWHILE, hashTable);
+    pushHashTable("If", TK_IF, hashTable);
+    pushHashTable("Then", TK_WITH, hashTable);
+    pushHashTable("Endif", TK_ENDIF, hashTable);
+    pushHashTable("Read", TK_READ, hashTable);
+    pushHashTable("Write", TK_WRITE, hashTable);
+    pushHashTable("Return", TK_RETURN, hashTable);
+    pushHashTable("Call", TK_CALL, hashTable);
+    pushHashTable("Record", TK_RECORD, hashTable);
+    pushHashTable("endrecord", TK_ENDRECORD, hashTable);
+    pushHashTable("Else", TK_ELSE, hashTable);
+}
+
+int main()
+{
+    pushKeyword(hashMap);
+    int coll = 0;
+    int total = 0;
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        hashEntry *curr = &hashMap[i];
+        if (curr->str != NULL)
+        {
+            while (curr && curr->str != NULL)
+            {
+                total++;
+                coll++;
+                printf("%s\n", curr->str);
+                curr = curr->next;
+            }
+            coll--;
+        }
+    }
+    printf("%d, %d", coll, total);
 }
--- hashTable.h
+++ hashTable.h
@@ -4,18 +4,25 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 #define BUFF_SIZE 4096
-#define TABLE_SIZE 1000
+#define TABLE_SIZE 20000
 #define MAX_ID_SIZE 20
+#define ll long long
 
 char buf1[BUFF_SIZE+1];
 char buf2[BUFF_SIZE+1];
 
+// Verify TK_GE
+
 typedef enum {
     TK_ASSIGNOP, TK_COMMENT, TK_FIELDID, TK_ID, TK_NUM, TK_RNUM, TK_FUNID, TK_RUID, TK_WITH, TK_PARAMETERS, TK_END, TK_WHILE, TK_UNION, TK_ENDUNION, TK_DEFINETYPE, TK_AS, TK_TYPE, 
     TK_MAIN, TK_GLOBAL, TK_PARAMETER, TK_LIST, TK_SQL, TK_SQR, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_COMMA, TK_SEM, TK_COLON, TK_DOT, TK_ENDWHILE, TK_OP, TK_CL, TK_IF, TK_THEN,
-    TK_ENDIF, TK_READ, TK_WRITE, TK_RETURN, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV, TK_CALL, TK_RECORD, TK_ENDRECORD, TK_ELSE, TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ, TK_GT, TK_LE,
+    TK_ENDIF, TK_READ, TK_WRITE, TK_RETURN, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV, TK_CALL, TK_RECORD, TK_ENDRECORD, TK_ELSE, TK_AND, TK_OR, TK_NOT, TK_LT, TK_LE, TK_EQ, TK_GT, TK_GE,
     TK_NE
 } Tk;
+
+typedef enum{
+    FALSE, TRUE
+} Bool;
 
 typedef struct entry{
     char* str; //the string corresponding to the lexeme
@@ -24,11 +31,20 @@ typedef struct entry{
 
 } hashEntry;
 
+typedef struct ret{
+    Bool found; //1 if the corresponding lexeme has been hashed
+    hashEntry* ent; //NULL if found is 0, otherwise the entry in the hashMap
+} hashRet;
+
+
 hashEntry hashMap[TABLE_SIZE];
 
 int hashCode(char* str);
 void constructHashTable(hashEntry hashTable[]);
 void pushHashTable(char* str, Tk tokenType, hashEntry hashTable[]);
 void pushKeyword(hashEntry hashTable[]);
+hashRet* initHashRet();
+hashRet* search(char* str, hashEntry hashMap[]);
+
 
 #endif
