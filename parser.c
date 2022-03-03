8f901c73f43852bf4915eb9855d7ce2db924c6d6
Sabhya Kapur <sabhya.1802@gmail.com>
2022-03-03 16:21:15 +0530

    stack functions

--- parser.c
+++ parser.c
@@ -1,8 +1,31 @@
 #include "parser.h"
 
+
+//some lines may be missing!
 StackEle* initStack(){
     StackEle* firstNode = (StackEle*)malloc(sizeof(StackEle));
     firstNode->next = NULL;
     firstNode->ele = (TreeNode*)malloc(sizeof(TreeNode));
     firstNode->ele->treeTag = LEAF;
+    firstNode->ele->node.leafNode.child = NULL;
+    firstNode->ele->node.leafNode.sibling = NULL;
 }
+
+StackEle* push(StackEle* node, StackEle* head){
+    node->next = head;
+    head = node;
+    return head;
+}
+
+StackEle* pop(StackEle* head){
+    if(!head){
+        return NULL;
+    }
+    else{
+        StackEle* rem = head;
+        head = head->next;
+        free(rem);
+        return head;
+    }
+}
+
