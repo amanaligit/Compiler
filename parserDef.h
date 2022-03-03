b811e82d837fcc3b780daa604da2d2b7279a907e
Aman Ali <ali.aman.2010@gmail.com>
2022-03-03 22:34:39 +0530

    parser

--- parserDef.h
+++ parserDef.h
@@ -3,33 +3,52 @@
 #ifndef PARSER_DEF_H
 #define PARSER_DEF_H
 
-typedef enum treeNodeTag{
-    NT, LEAF
+typedef enum treeNodeTag
+{
+    NT,
+    TERM
 } TreeNodeTag;
 
-typedef struct ntNode{
+#define MAX_TERMS 64
+
+typedef struct grammar
+{
+    int productions[MAX_TERMS][MAX_TERMS][MAX_TERMS];
+    int first[MAX_TERMS][MAX_TERMS];
+    int follow[MAX_TERMS][MAX_TERMS];
+
+    // 0: [[1, 2, 3], [2, 4, 5], ]
+
+} Grammar;
+
+typedef struct ntNode
+{
     Tk tokenType;
     TreeNode *child, *sibling, *parent;
 } NTNode;
 
-typedef struct leafNode{
+typedef struct leafNode
+{
     Token fromLexer;
-    TreeNode* child, *sibling, *parent;
+    TreeNode *child, *sibling, *parent;
 } LeafNode;
 
-typedef union combine{
-        NTNode ntNode;
-        LeafNode leafNode;
-    } Combine;
+typedef union combine
+{
+    NTNode ntNode;
+    LeafNode leafNode;
+} Combine;
 
-typedef struct treeNode{
+typedef struct treeNode
+{
     Combine node;
     TreeNodeTag treeTag;
-}TreeNode;
+} TreeNode;
 
-typedef struct stackEle{
-    TreeNode* ele;
-    struct stackEle* next;
+typedef struct stackEle
+{
+    TreeNode *ele;
+    struct stackEle *next;
 } StackEle;
 
 #endif
